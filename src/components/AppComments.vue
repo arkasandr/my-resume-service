ccc5ee72a4ca4840c71972b3e351e73278b7e8d4
arkasandr <arkasandr@gmail.com>
2021-01-14 00:14:28 +0300

    add comments component

---
+++ src/components/AppComments.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="card">
+    <h2>Комментарии</h2>
+    <ul class="list">
+      <li class="list-item" v-for="item in comments" :key="item.id">
+        <div>
+          <p><strong>{{ item.email }}</strong></p>
+          <small>{{item.body}}</small>
+        </div>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'AppComments',
+
+  props: [
+    'comments'
+  ]
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/components/AppTitle.vue
@@ -0,0 +1,13 @@
+<template>
+    <h1>This is AppTitle </h1>
+</template>
+
+<script>
+export default {
+  name: 'AppTitle'
+}
+</script>
+
+<style scoped>
+
+</style>
